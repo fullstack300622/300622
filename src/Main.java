c02b20b9b87c74c7bf19ea9c72f9122ae884d3f2
fullstack300622 <fullstack300622@gmail.com>
2022-06-30 20:15:20 +0300

    306022

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 0;
+        int secondNumber = 0;
+
+        float fqq = 5.5f;
+        double d = 5.5;
+        String name = "";
+        char latter = 'a';
+        String myText = "Hello world";
+
+        System.out.println(myText);
+        System.out.println(myText);
+        System.out.println(myText);
+        System.out.println(myText);
+        System.out.println(myText);
+
+
+    }
+
+//    public static void main(String[] args) {
+//        //create input from keyboard
+//        Scanner s= new Scanner(System.in);
+//
+//        //get input from keyboard
+//        String my_name = s.nextLine();
+//
+//        System.out.print("Hello ");
+//        System.out.println(my_name);
+//    }
+}
